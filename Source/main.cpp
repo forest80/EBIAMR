0ec6fe592e7f55267a1e3c34bd27281907e56d7b
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-01-30 09:59:51 -0800

    build with eb source code

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,15 +1,15 @@
 
-#include <cstdio>
-
-#include <AMReX_CArena.H>
-#include <AMReX_REAL.H>
-#include <AMReX_Utility.H>
-#include <AMReX_Amr.H>
+#include <AMReX.H>
 #include <AMReX_ParmParse.H>
 #include <AMReX_ParallelDescriptor.H>
-#include <AMReX_BLProfiler.H>
+#include <AMReX_Amr.H>
+#include <AMReX_EBTower.H>
+
+#include <NavierStokesBase.H>
 
 using namespace amrex;
+
+void initialize_EBIS(const int max_level);
 
 int
 main (int   argc,
@@ -20,7 +20,9 @@ main (int   argc,
     BL_PROFILE_REGION_START("main()");
     BL_PROFILE_VAR("main()", pmain);
 
-    const Real run_strt = ParallelDescriptor::second();
+    Real timer_tot = ParallelDescriptor::second();
+    Real timer_init = 0.;
+    Real timer_advance = 0.;
 
     int  max_step;
     int  num_steps;
@@ -49,67 +51,82 @@ main (int   argc,
         amrex::Abort("Exiting because neither max_step nor stop_time is non-negative.");
     }
 
-    Amr* amrptr = new Amr;
-
-    amrptr->init(strt_time,stop_time);
-
-    if (num_steps > 0)
     {
-        if (max_step < 0)
+        timer_init = ParallelDescriptor::second();
+
+        Amr amr;
+
+        initialize_EBIS(amr.maxLevel());
+        EBTower::Build();
+        AMReX_EBIS::reset();  // We no longer needs the EBIndexSpace singleton.
+        AmrLevel::SetEBSupportLevel(EBSupport::full);
+        AmrLevel::SetEBMaxGrowCells(5,4,2);
+
+        amr.init(strt_time,stop_time);
+
+        if (num_steps > 0)
         {
-            max_step = num_steps + amrptr->levelSteps(0);
+            if (max_step < 0)
+            {
+                max_step = num_steps + amr.levelSteps(0);
+            }
+            else
+            {
+                max_step = std::min(max_step, num_steps + amr.levelSteps(0));
+            }
+
+            amrex::Print() << "Using effective max_step = " << max_step << '\n';
         }
-        else
+        //
+        // If we set the regrid_on_restart flag and if we are *not* going to take
+        // a time step then we want to go ahead and regrid here.
+        //
+        if (amr.RegridOnRestart())
         {
-            max_step = std::min(max_step, num_steps + amrptr->levelSteps(0));
+            if (    (amr.levelSteps(0) >= max_step ) ||
+                    ( (stop_time >= 0.0) &&
+                      (amr.cumTime() >= stop_time)  )    )
+            {
+                //
+                // Regrid only!
+                //
+                amr.RegridOnly(amr.cumTime());
+            }
         }
 
-	amrex::Print() << "Using effective max_step = " << max_step << '\n';
-    }
-    //
-    // If we set the regrid_on_restart flag and if we are *not* going to take
-    // a time step then we want to go ahead and regrid here.
-    //
-    if (amrptr->RegridOnRestart())
-    {
-        if (    (amrptr->levelSteps(0) >= max_step ) ||
-                ( (stop_time >= 0.0) &&
-                  (amrptr->cumTime() >= stop_time)  )    )
+        timer_init = ParallelDescriptor::second() - timer_init;
+        timer_advance = ParallelDescriptor::second();
+
+        while ( amr.okToContinue()                            &&
+                (amr.levelSteps(0) < max_step || max_step < 0) &&
+                (amr.cumTime() < stop_time || stop_time < 0.0) )
         {
-            //
-            // Regrid only!
-            //
-            amrptr->RegridOnly(amrptr->cumTime());
+            amr.coarseTimeStep(stop_time);
         }
+
+        timer_advance = ParallelDescriptor::second() - timer_advance;
+
+        if (amr.stepOfLastCheckPoint() < amr.levelSteps(0))
+        {
+            amr.checkPoint();
+        }
+
+        if (amr.stepOfLastPlotFile() < amr.levelSteps(0))
+        {
+            amr.writePlotFile();
+        }
+
+        EBTower::Destroy();
     }
 
-    while ( amrptr->okToContinue()                            &&
-           (amrptr->levelSteps(0) < max_step || max_step < 0) &&
-           (amrptr->cumTime() < stop_time || stop_time < 0.0) )
-    {
-        amrptr->coarseTimeStep(stop_time);
-    }
-    //
-    // Write final checkpoint and plotfile.
-    //
-    if (amrptr->stepOfLastCheckPoint() < amrptr->levelSteps(0))
-    {
-        amrptr->checkPoint();
-    }
+    timer_tot = ParallelDescriptor::second() - timer_tot;
 
-    if (amrptr->stepOfLastPlotFile() < amrptr->levelSteps(0))
-    {
-        amrptr->writePlotFile();
-    }
+    ParallelDescriptor::ReduceRealMax({timer_tot, timer_init, timer_advance},
+                                      ParallelDescriptor::IOProcessorNumber());
 
-    delete amrptr;
-
-    const int IOProc   = ParallelDescriptor::IOProcessorNumber();
-    Real      run_stop = ParallelDescriptor::second() - run_strt;
-
-    ParallelDescriptor::ReduceRealMax(run_stop,IOProc);
-
-    amrex::Print() << "Run time = " << run_stop << std::endl;
+    amrex::Print() << "Run Time total        = " << timer_tot     << "\n"
+                   << "Run Time init         = " << timer_init    << "\n"
+                   << "Run Time advance      = " << timer_advance << "\n";
 
     BL_PROFILE_VAR_STOP(pmain);
     BL_PROFILE_REGION_STOP("main()");
